f05c789f9fe2ac3c174df5262878c4220bf64b6a
Kunal Singhal <knsn1994@gmail.com>
2014-08-17 04:11:07 +0530

    0.751379

--- tok.py
+++ tok.py
@@ -4,6 +4,7 @@ for line in sys.stdin:
     line = line.strip()
     print line
     line = line.replace("'s", " 's")
+    line = line.replace("...", " ...")
     print len(line.split())
     for x in line.split():
         print x
